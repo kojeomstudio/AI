b6a9ae0f3711716d3ffd727f334f364667712d57
kojeomstudio <kojeomstudio@gmail.com>
2024-08-01 22:06:38 +0900

    # 코드 수정.

--- Scripts/study/classify_wine.py
+++ Scripts/study/classify_wine.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split # 데이터셋 분리에 사용.
 
-from sklearn.linear_model import LinearRegression # 로지스틱 회귀 ( 회귀라는 이름이지만 실제로는 분류에 사용 -> 다항식 )
+from sklearn.linear_model import LogisticRegression # 로지스틱 회귀 ( 회귀라는 이름이지만 실제로는 분류에 사용 -> 다항식 )
 from sklearn.linear_model import Ridge # 특성에 대한 규제를 하는 릿지
 from sklearn.svm import SVC
 
@@ -29,7 +29,8 @@ print("{0}".format(wine_data.describe()))
 # 특성과 레이블 분리 (예시로 'quality'를 레이블로 사용)
 # 여러 특성을 이용해서 와인의 '품질'을 예측한다. 라는 문제로 생각해본다.
 # 
-X = wine_data.drop('quality', axis=1) # quality 컬럼을 제외한 나머지는 feature (=학습에 이용)
+X = wine_data.drop('Id', axis=1) # 불필요한 컬럼 제외.
+X = X.drop('quality', axis=1) # quality 컬럼을 제외한 나머지는 feature (=학습에 이용)
 y = wine_data['quality'] # quality는 라벨링 (=정답)
 
 # 데이터 나누기
@@ -46,13 +47,12 @@ print(f'Training set: {x_train.shape}, {y_train.shape}')
 print(f'Validation set: {x_val.shape}, {y_val.shape}')
 print(f'Test set: {x_test.shape}, {y_test.shape}')
 
-lr = LinearRegression()
-lr.fit(x_train, y_train)
+logistciReg = LogisticRegression()
+logistciReg.fit(x_train, y_train)
 
-print("linearRegression -> coef : {0}".format(lr.coef_))
+print("logisticRegression -> coef : {0}".format(logistciReg.coef_))
 
-print("linearRegression -> score : {0}".format(lr.score(x_test, y_test)))
-#print("linearRegression -> predict : {0}".format(lr.predict(x_test)))
+print("logisticRegression -> score : {0}".format(logistciReg.score(x_test, y_test)))
 
 # SVM 사용
 # SVM 모델 초기화 및 학습
