3e2e8943c5201b060cfbc462316fc0454c041cb7
kojeomstudio <kojeomstudio@gmail.com>
2024-10-02 20:56:00 +0900

    # 코드 수정.

--- Study/clustering_fruits.py
+++ Study/clustering_fruits.py
@@ -3,17 +3,16 @@ import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 
-friuits = np.load('./Datas/fruits.npy')
-friuits_2d = friuits.reshape(-1, 128 * 128) # 128 x 128 size image
+# KMeans 에서 사용할 수 있게 2차원 배열 형태로 수정.
+fruits = np.load('./Datas/fruits.npy') # RGBA 4차원 원소
+fruits_2d = fruits.reshape(-1, 128 * 128) # 128 x 128 size image
 #print(friuits)
-
-print(f"labels info : {np.unique(km.labels_, return_counts=True)}")
 
 # 군집에 필요한 k 값 찾기. ( 엘보 ) ==================
 inertia = []
 for k_value in range(2, 7):
     km = KMeans(n_clusters=k_value, n_init='auto', random_state=42)
-    km.fit(friuits_2d)
+    km.fit(fruits_2d)
     inertia.append(km.inertia_)
 
 plt.plot(range(2, 7), inertia)
@@ -24,4 +23,6 @@ plt.show()
 
 # elbow 방법으로 찾은 값은 4로 나옴. (과일 데이터 기준))
 km = KMeans(n_clusters=4, random_state=42)
-km.fit(friuits_2d)
+km.fit(fruits_2d)
+
+print(f"labels info : {np.unique(km.labels_, return_counts=True)}")
