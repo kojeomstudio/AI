e862fcfd8664042b7c903ce4ed7ef646b30d2769
kojeomstudio <kojeomstudio@gmail.com>
2024-10-27 10:45:09 +0900

    # 파일 경로 변경 / 테스트 작업.

---
+++ llm/rag/r2r_framework/r2r_framework_sample.py
@@ -0,0 +1,10 @@
+
+# r2r 프레임워크 테스트 코드
+
+# pip install r2r
+# r2r serve --docker --full --config-name=full_local_llm
+
+# r2r serve --docker --full --config-path=/home/kojeomstudio/AI/llm/rag/my_r2_config.toml 
+# (path는 도커 이미지 내 경로 혹은 share 인것 같음.)
+
+# r2r docker-down
