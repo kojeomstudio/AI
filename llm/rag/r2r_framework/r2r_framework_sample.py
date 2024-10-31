30daa7e98e42005d9cf91b041d3e2f4a7967ac5c
kojeomstudio <kojeomstudio@gmail.com>
2024-10-31 09:50:34 +0900

    # r2r 프레임워크 관련 테스트 중..

--- llm/rag/r2r_framework/r2r_framework_sample.py
+++ llm/rag/r2r_framework/r2r_framework_sample.py
@@ -1,10 +1,14 @@
 
 # r2r 프레임워크 테스트 코드
 
-# pip install r2r
+# pip install -U r2r
+# pip install 'r2r[core,ingestion-bundle]'
+
+# r2r update
+
 # r2r serve --docker --full --config-name=full_local_llm
 
-# r2r serve --docker --full --config-path=/home/kojeomstudio/AI/llm/rag/my_r2_config.toml 
+# r2r serve --docker --full --config-path=/Users/kojeomstudio/AI/llm/rag/r2r_framework/my_r2_config.toml 
 # (path는 도커 이미지 내 경로 혹은 share 인것 같음.)
 
 # r2r docker-down
