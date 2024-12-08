8fcd5d8818d78fc0596e248183f74840886ede5a
kojeomstudio <kojeomstudio@gmail.com>
2024-12-08 09:43:21 +0900

    # ollama 모델 파일 추가 / r2r config 분리 및 수정.

---
+++ llm/rag/r2r_framework/instructions.py
@@ -0,0 +1,17 @@
+
+# r2r 프레임워크 테스트 코드
+
+# pip install -U r2r
+# pip install 'r2r[core,ingestion-bundle]'
+
+# r2r update
+
+# r2r serve --docker --full --config-name=full_local_llm
+
+# 풀 버전
+# r2r serve --docker --full --config-path=/Users/kojeomstudio/AI/llm/rag/r2r_framework/full_r2r_config.toml
+
+# 심플 버전
+# r2r serve --docker --config-path=/Users/kojeomstudio/AI/llm/rag/r2r_framework/light_r2r_config.toml
+
+# r2r docker-down
