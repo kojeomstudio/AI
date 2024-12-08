00742e7f8efaa31d8c598a1f7148dc378179b09b
kuma <kojeomstudio@gmail.com>
2024-12-08 10:39:50 +0900

    # 기종 별로 config 분리.

--- llm/rag/r2r_framework/instructions.py
+++ llm/rag/r2r_framework/instructions.py
@@ -8,10 +8,8 @@
 
 # r2r serve --docker --full --config-name=full_local_llm
 
-# 풀 버전
-# r2r serve --docker --full --config-path=/Users/kojeomstudio/AI/llm/rag/r2r_framework/full_r2r_config.toml
-
-# 심플 버전
-# r2r serve --docker --config-path=/Users/kojeomstudio/AI/llm/rag/r2r_framework/light_r2r_config.toml
+# 맥북 m3 프로 기준
+# r2r serve --docker --full --config-path=/Users/kojeomstudio/AI/llm/rag/r2r_framework/config/m3-macbook-pro/full_r2r_config.toml
+# r2r serve --docker --config-path=/Users/kojeomstudio/AI/llm/rag/r2r_framework/config/m3-macbook-pro/light_r2r_config.toml
 
 # r2r docker-down
