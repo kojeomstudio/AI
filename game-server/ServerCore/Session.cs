92274ba3da7929b1538ca9e309d2502f078411df
kojeomstudio <kojeomstudio@gmail.com>
2025-08-02 17:56:57 +0900

    feat : 작업.

--- game-server/ServerCore/Session.cs
+++ game-server/ServerCore/Session.cs
@@ -12,21 +12,35 @@ namespace ServerCore
         private Socket _socket;
         private int _disconnected = 0;
 
+        private SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
+        private object _lock = new object();
+        private Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        private bool _pending = false;
+
         public void Start(Socket socket)
         {
             _socket = socket;
 
             SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
-
             recvArgs.SetBuffer(new byte[1024], 0, 1024);
+
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
             RegisterRecv(recvArgs);
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);
+            lock(_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pending == false)
+                {
+                    RegisterSend();
+                }
+            }
         }
 
         public void Disconnect()
@@ -41,6 +55,50 @@ namespace ServerCore
         }
 
         #region Network
+        private void RegisterSend()
+        {
+            _pending = true;
+
+            byte[] buff = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if(pending == false)
+            {
+                OnSendCompleted(null, _sendArgs);
+            }
+        }
+
+        private void OnSendCompleted(object? sender, SocketAsyncEventArgs args)
+        {
+            lock(_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        if (_sendQueue.Count > 0)
+                        {
+                            RegisterSend();
+                        }
+                        else
+                        {
+                            _pending = false;
+                        }
+                            
+                    }
+                    catch (Exception ex)
+                    {
+                        ServerLogger.Instance.Log(LogLevel.Error, $"OnSendCompleted Faield : {ex.ToString()}");
+                    }
+                }
+                else
+                {
+                    //ServerLogger.Instance.Log(LogLevel.Error, $"OnSendCompleted Failed : {args.SocketError}");
+                    Disconnect();
+                }
+            }
+        }
         private void RegisterRecv(SocketAsyncEventArgs args)
         {
             bool pending = _socket.ReceiveAsync(args);
@@ -57,7 +115,7 @@ namespace ServerCore
                 try
                 {
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
-                    Console.WriteLine($"[From Client] {recvData}");
+                    ServerLogger.Instance.Log(LogLevel.Info, $"[From Client] {recvData}");
                     RegisterRecv(args);
                 }
                 catch (Exception ex)
@@ -67,7 +125,7 @@ namespace ServerCore
             }
             else
             {
-
+                Disconnect();
             }
         }
         #endregion
