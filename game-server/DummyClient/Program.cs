0d4d74a2ca1f121a8a71ee7971d6087f167b18c9
KOJEOMSTUDIO\kojeo <kojeomstudio@gmail.com>
2026-01-10 00:43:07 +0900

    feat : 서버 작업.

--- game-server/DummyClient/Program.cs
+++ game-server/DummyClient/Program.cs
@@ -7,55 +7,6 @@ namespace DummyClient
 {
     internal class Program
     {
-        public class Packet
-        {
-            public ushort size;
-            public ushort packetId;
-        }
-
-        class GameSession : Session
-        {
-            public override void OnConnected(EndPoint endPoint)
-            {
-                Console.WriteLine($"OnConnected EndPoint : {endPoint}");
-
-                Packet packet = new Packet() { size = 100, packetId = 10 };
-
-                for (int i = 0; i < 5; i++)
-                {
-                    ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-
-                    byte[] sizeBuffer = BitConverter.GetBytes(packet.size);
-                    byte[] packetIdBuffer = BitConverter.GetBytes(packet.packetId);
-
-                    Array.Copy(sizeBuffer, 0, openSegment.Array, openSegment.Offset, sizeBuffer.Length);
-                    Array.Copy(packetIdBuffer, 0, openSegment.Array, openSegment.Offset + sizeBuffer.Length, packetIdBuffer.Length);
-                    ArraySegment<byte> sendBuffer = SendBufferHelper.Close(packet.size);
-
-                    Send(sendBuffer);
-
-                    ClientLogger.Instance.Info($"Sent {sendBuffer.Count} bytes to server, index : {i}");
-                }
-            }
-
-            public override void OnDisconnected(EndPoint endPoint)
-            {
-                Console.WriteLine($"OnDisconnected EndPoint : {endPoint}");
-            }
-
-            public override int OnRecv(ArraySegment<byte> buffer)
-            {
-
-                string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
-                Console.WriteLine($"[From Server] {recvData}");
-                return buffer.Count;
-            }
-
-            public override void OnSend(int numOfBytes)
-            {
-                Console.WriteLine($"Transferred Bytes : {numOfBytes}");
-            }
-        }
         static void Main(string[] args)
         {
             string host = Dns.GetHostName();
@@ -68,7 +19,7 @@ namespace DummyClient
              );
 
             Connector connector = new Connector();
-            connector.Connect(endPoint, () => { return new GameSession(); });
+            connector.Connect(endPoint, () => { return new ServerSession(); });
 
             while (true)
             {
--- game-server/Server/Program.cs
+++ game-server/Server/Program.cs
@@ -1,59 +1,11 @@
-﻿using ServerCore;
+﻿using Server;
+using ServerCore;
 using System.Net;
 using System.Text;
 
 namespace KojeomGameServer
 {
-    public class Packet
-    {
-        public ushort size;
-        public ushort packetId;
-    }
-
-    class GameSession : PacketSession
-    {
-        public override void OnConnected(EndPoint endPoint)
-        {
-            ServerLogger.Instance.Log(LogLevel.Info, $"OnConnected EndPoint : {endPoint}");
-
-            //Packet packet = new Packet() { size = 100, packetId = 10 };
-
-            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-
-            //byte[] sizeBuffer = BitConverter.GetBytes(packet.size);
-            //byte[] packetIdBuffer = BitConverter.GetBytes(packet.packetId);
-
-            //Array.Copy(sizeBuffer, 0, openSegment.Array, openSegment.Offset, sizeBuffer.Length);
-            //Array.Copy(packetIdBuffer, 0, openSegment.Array, openSegment.Offset + sizeBuffer.Length, packetIdBuffer.Length);
-            //ArraySegment<byte> sendBuffer = SendBufferHelper.Close(packet.size);
-
-            //Send(sendBuffer);
-
-            Thread.Sleep(5000);
-
-            Disconnect();
-        }
-
-        public override void OnDisconnected(EndPoint endPoint)
-        {
-            ServerLogger.Instance.Log(LogLevel.Info, $"OnDisconnected EndPoint : {endPoint}");
-        }
-
-        public override void OnRecvPacket(ArraySegment<byte> buffer)
-        {
-            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-            ushort packetId = BitConverter.ToUInt16(buffer.Array, buffer.Offset + PacketSession.HeaderSize);
-            
-            ServerLogger.Instance.Log(LogLevel.Info, $"RecvPacketId : {packetId}, Size : {size}");
-        }
-
-        public override void OnSend(int numOfBytes)
-        {
-            ServerLogger.Instance.Log(LogLevel.Info, $"Transferred Bytes : {numOfBytes}");
-        }
-    }
-
-    internal class Program
+    public class Program
     {
         static Listener _listener = new Listener();
 
@@ -68,7 +20,7 @@ namespace KojeomGameServer
                 7777
              );
 
-            _listener.Init(endPoint, () => { return new GameSession(); });
+            _listener.Init(endPoint, () => { return new ClientSession(); });
 
             while (true)
             {
